58df15be248bf088e505455675ae4ddfaeb6d701
Пётр <klepcin@mobak.ru>
2017-11-21 14:17:00 +0500

    AirDrop logic and data added

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
+const data = require('./data.json');
 const Web3 = require('web3')
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545/'));
-// const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/'));
 
 const LCAddress = '0xD7F82E36ef04c9543ce18e6D1072354a1b9D629c';
 const fromAddress = '0x7f30a87203b56347f78e2a8f8a98a5297c0d3dc0';
-const toAddress = '0xE41619781aC922aB06884019122010A6ce00756e';
 const LCABI = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"INITIAL_SUPPLY","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"burn","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Burn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}];
 const LC = new web3.eth.Contract(LCABI, LCAddress);
 const _1LC = 1000000000000000000;
 
-const job = async () => {
-	await LC.methods.transfer(toAddress, _1LC / 1000).send({from: fromAddress, gasPrice: '100000000'});
+const wallet = data['wallet'];
+const res = Object.keys(wallet).reduce((acc, ind) => acc + wallet[ind].totalLC, 0);
+console.log(res);
+let count = 0;
 
-	const fromLC = (await LC.methods.balanceOf(fromAddress).call()).toNumber();
-	const toLC = (await LC.methods.balanceOf(toAddress).call()).toNumber();
-
-	console.log(`fromLC = ${fromLC} toLC = ${toLC}`);
-};
-
-job();
+for (const p in wallet) {
+	const obj = wallet[p];
+	const toAddress = obj.ethAddress;
+	if (obj.totalLC > 0 && toAddress != null && toAddress.startsWith('0x') && toAddress.length == 42) {
+		LC.methods.transfer(toAddress, _1LC * obj.totalLC).send({from: fromAddress, gasPrice: '100000000'});
+		count++;
+	} else {
+		console.log(obj);
+	}
+}
+console.log(`total count = ${count}`);
